7c9b3af98a9bec3d634108566fe84b6dac66b4f5
Mshita118 <t.mshita118@gmail.com>
2025-03-10 22:41:10 +0900

    windowの作成

---
+++ script.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import messagebox
+import mysql.connector
+
+root = Tk()
+
+root.title("請求書自動化アプリ")
+root.geometry("800x400")  # 幅を広げる
+root.maxsize(1024, 576)
+root.minsize(640, 360)
+
+
+root.mainloop()
